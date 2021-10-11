d8998519f3037a0318234c4bae93a28f7119341e
RobiulMan <robiulislam.bg@gmail.com>
2021-10-12 00:38:01 +0600

    init

---
+++ src/index.ts
@@ -0,0 +1,11 @@
+import { Company } from "./Company";
+import { User } from "./User";
+
+const title = document.querySelector(".title");
+
+const user = new User();
+console.log(user);
+const company = new Company();
+console.log(company);
+
+console.log("we are runing...");
